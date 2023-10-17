cf619bfa98df59f7f4b93b3f10a3cfc740ac09e2
DEVitor0 <133174280+DEVitor0@users.noreply.github.com>
2023-10-17 15:53:05 -0300

    resolução do problema para o menu hamburguinho
    após abrir o menu hamburguer, existia um texto (h1 e p) sobrepondo as letras dos itens do menu de navegação

--- assets/celular.js
+++ assets/celular.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const menuList = document.querySelector('.menu-list');
+const menuHiddenElements = document.querySelectorAll('.menu-hidden');
 
 hamburger.addEventListener('click', () => {
     menuList.classList.toggle('active');
     hamburger.classList.toggle('active');
+
+    menuHiddenElements.forEach((element) => {
+        element.style.zIndex = menuList.classList.contains('active') ? -1 : 2;
+    });
 });
