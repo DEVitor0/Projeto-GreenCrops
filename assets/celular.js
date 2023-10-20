b555e49c4a6f77c83f14e6f0f6ede431cb6051af
DEVitor0 <133174280+DEVitor0@users.noreply.github.com>
2023-10-19 23:50:41 -0300

    quebrando a cabeça junto com o GPT para arrumar a caixa
    tentando fazer com que o conteúdo ao ser clicado no icone ele exiba ao lado esquerdo e o icone a direita

--- assets/celular.js
+++ assets/celular.js
@@ -22,3 +22,36 @@ function adjustTextPosition() {
 window.addEventListener('resize', adjustTextPosition);
 adjustTextPosition();
 
+function toggleItem(item) {
+    const itemSelecionado = document.getElementById('item-selecionado');
+    const selectedIcon = document.getElementById('selected-icon');
+    const selectedText = document.getElementById('selected-text');
+    
+    if (itemSelecionado.style.display === 'none' || itemSelecionado.style.display === '') {
+        // Exibe o ícone e o texto selecionados
+        selectedIcon.textContent = item.querySelector('.item-icon').textContent.trim();
+        selectedText.textContent = item.querySelector('.item-text').textContent;
+        itemSelecionado.style.display = 'flex';
+        
+        // Esconde os outros itens
+        const todosItens = document.querySelectorAll('.itens');
+        todosItens.forEach(i => {
+            if (i !== item) {
+                i.style.display = 'none';
+            }
+        });
+
+        // Adiciona a classe "expandido" para animar o esticamento
+        item.classList.add('expandido');
+        selectedIcon.style.opacity = 1; // Mostra o ícone esticado
+    } else {
+        // Recolhe o conteúdo e restaura os outros itens
+        itemSelecionado.style.display = 'none';
+        const todosItens = document.querySelectorAll('.itens');
+        todosItens.forEach(i => {
+            i.style.display = 'flex';
+            i.classList.remove('expandido');
+            i.querySelector('.item-icon').style.opacity = 1; // Mostra todos os ícones novamente
+        });
+    }
+}
